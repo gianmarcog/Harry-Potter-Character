4d81408975cfd1f2c3f10976fe606969089048fa
gianmarcog <marco.giretti@web.de>
2019-12-03 02:05:52 +0100

    Initial Commit

---
+++ Harry Potter Character/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Harry Potter Character
+//
+//  Created by Gianmarco Giretti on 03.12.19.
+//  Copyright © 2019 Gianmarco Giretti. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
