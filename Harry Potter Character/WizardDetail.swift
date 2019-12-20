0644f38c7b6e864eeca94edfecb1b0f761d40a31
gianmarcog <marco.giretti@web.de>
2019-12-20 17:07:09 +0100

    Change class name

--- Harry Potter Character/WizardDetail.swift
+++ Harry Potter Character/WizardDetail.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  WizardDetail.swift
 //  Harry Potter Character
 //
 //  Created by Gianmarco Giretti on 03.12.19.
@@ -8,7 +8,9 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct WizardDetail: View {
+    
+    var wizard : Wizard
     var body: some View {
         VStack{
             // VStack create a vertical line where the images/texts are following
@@ -22,14 +24,16 @@ struct ContentView: View {
             // The Sapcer takes as much space as he can. Sof if you put him on the bottom of the VStack it pushing the images to the top
             
             // edgesIgnoringSafeArea ignores the safe areas from Swift so you put your images wherever you want
-                Image("Gryffindor").resizable().aspectRatio(contentMode: .fit)
-        
-            Image("PotterHarry").clipShape(Circle()).overlay(Circle().stroke(Color.white, lineWidth: 4)).offset(x:0, y:-90).padding(.bottom, -90).shadow(radius: 20)
+                
+            Image(wizard.team.imageName).resizable().aspectRatio(contentMode: .fit)
+            Image(wizard.imageName).clipShape(Circle()).background(Circle().foregroundColor(.white)).overlay(Circle().stroke(Color.white, lineWidth: 4)).offset(x:0, y: -90).padding(.bottom, -70).shadow(radius: 15)
             
             // With the font function you can change the size
-            Text("Harry Potter").font(.system(size: 40)).fontWeight(.heavy)
+            Text(wizard.name).font(.system(size: 40)).fontWeight(.heavy)
             
-            StatText()
+            StatText(statName: "Feature ", statValue: wizard.feature)
+            StatText(statName: "House: ", statValue: wizard.house)
+            StatText(statName: "Hobby: ", statValue: wizard.hobby)
             Spacer()
         }.edgesIgnoringSafeArea(.top)
     }
@@ -37,6 +41,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        WizardDetail(wizard: wizards[0])
     }
 }
