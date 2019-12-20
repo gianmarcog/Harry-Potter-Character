203a7e571b6dd67a70ee95fe0932f418abfb9582
gianmarcog <marco.giretti@web.de>
2019-12-20 03:05:35 +0100

    Create the person page

--- Harry Potter Character/ContentView.swift
+++ Harry Potter Character/ContentView.swift
@@ -10,7 +10,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Image("Gryffindor")
+        VStack{
+            // VStack create a vertical line where the images/texts are following
+            
+            // resizable allows to streched and shrunk time image
+            
+            // aspectRatio allows to completely fill an image with the appropriate one or to fit the image to the width and height
+            
+            // clipShape make the image to a circle. overlay make the outline color. offset changes the position insde the VStack line
+            
+            // The Sapcer takes as much space as he can. Sof if you put him on the bottom of the VStack it pushing the images to the top
+            
+            // edgesIgnoringSafeArea ignores the safe areas from Swift so you put your images wherever you want
+                Image("Gryffindor").resizable().aspectRatio(contentMode: .fit)
+        
+            Image("PotterHarry").clipShape(Circle()).overlay(Circle().stroke(Color.white, lineWidth: 4)).offset(x:0, y:-90).padding(.bottom, -90).shadow(radius: 20)
+            
+            // With the font function you can change the size
+            Text("Harry Potter").font(.system(size: 40)).fontWeight(.heavy)
+            
+            StatText()
+            Spacer()
+        }.edgesIgnoringSafeArea(.top)
     }
 }
 
